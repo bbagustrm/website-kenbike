b015c09f9d80a3857fa9bd0dfbc6da3f48b7e628
Bagus Tri Atmojo <bbagustrm@gmail.com>
2024-04-29 14:09:51 +0700

    navbar+footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,php,js}'],
+  content: ['./public/**/*.{php,html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#09090B',
+        'text': '#27272A',
+        'textun': '#71717A',
+        'surface': '#FAFAFA',
+        'background': '#FFFFFF',
+
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
